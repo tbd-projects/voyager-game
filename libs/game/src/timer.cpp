2649d3eb73a04826b94d9a34ee04c6f4cc1cc5ff
Dmitry Varin <varindv@gmail.com>
2021-05-20 11:25:20 +0300

    fix timer-namespace

--- libs/game/src/timer.cpp
+++ libs/game/src/timer.cpp
@@ -3,14 +3,14 @@
 //
 #include "timer.hpp"
 
-void Timer::start() {
+void Game::Timer::start() {
     if (!_is_running) {
         this->_start = std::chrono::steady_clock::now();
         this->_is_running = true;
     }
 }
 
-void Timer::stop() {
+void Game::Timer::stop() {
     if (_is_running) {
         this->_duration = std::chrono::duration_cast<std::chrono::milliseconds>(
                 std::chrono::steady_clock::now() - this->_start);
@@ -18,20 +18,20 @@ void Timer::stop() {
     }
 }
 
-void Timer::reset() {
+void Game::Timer::reset() {
     this->_pause_sum = std::chrono::milliseconds(0);
     this->_is_running = false;
     this->_is_pause = false;
     this->_duration = std::chrono::milliseconds(0);
 }
 
-void Timer::pause() {
+void Game::Timer::pause() {
     this->_pause = std::chrono::steady_clock::now();
     this->_is_running = false;
     this->_is_pause = true;
 }
 
-void Timer::unpause() {
+void Game::Timer::unpause() {
     if (this->_is_pause) {
         this->_pause_sum = this->_pause_sum + std::chrono::duration_cast<std::chrono::milliseconds>(
                 std::chrono::steady_clock::now() - this->_pause);
@@ -40,21 +40,21 @@ void Timer::unpause() {
     }
 }
 
-std::chrono::milliseconds Timer::get_ms() {
+std::chrono::milliseconds Game::Timer::get_ms() {
     this->unpause();
     this->stop();
     return this->_duration - this->_pause_sum;
 }
 
-std::chrono::seconds Timer::get_s() {
+std::chrono::seconds Game::Timer::get_s() {
     return std::chrono::duration_cast<std::chrono::seconds>(this->get_ms());
 }
 
-bool Timer::is_pause() {
+bool Game::Timer::is_pause() {
     return this->_is_pause;
 }
 
-bool Timer::is_run() {
+bool Game::Timer::is_run() {
     return this->_is_running;
 }
 
